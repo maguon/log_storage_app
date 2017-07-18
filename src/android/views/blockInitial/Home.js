0d3ed98795ece8c73fa6b7a4f8c4a1c0f8f60cb6
rb yu <rbyu@rbdeMacBook-Air.local>
2017-07-18 16:33:43 +0800

    ok

--- src/android/views/blockInitial/Home.js
+++ src/android/views/blockInitial/Home.js
@@ -12,9 +12,14 @@ class Home extends Component {
         this.onPressIcon = this.onPressIcon.bind(this)
         this.onPressTextInput = this.onPressTextInput.bind(this)
         this.onPressItem = this.onPressItem.bind(this)
+        this.getData = this.getData.bind(this)
     }
 
     componentDidMount() {
+        this.getData()
+    }
+
+    getData() {
         let now = new Date()
         year = now.getFullYear()
         month = now.getMonth() + 1
@@ -27,12 +32,12 @@ class Home extends Component {
         let { userId } = this.props.userReducer
         getRecordsForHome({ OptionalParam: { start: 0, size: 10, userId: userId } })
         getStoragesForHome({ OptionalParam: { dateStart: now, dateEnd: now } })
-
     }
 
     componentWillReceiveProps(nextProps) {
-        let { homeReducer } = nextProps
+        let { homeReducer, CarInfoReducer, AddCarReducer } = nextProps
         let { resetStoragesForHome, resetRecordsForHome } = this.props
+        // console.log(nextProps)
         /** homeReducer.getStoragesHome */
         if (homeReducer.getStoragesHome.isExecStatus == 1) {
             console.log('homeReducer.getStoragesHome', '开始执行')
@@ -71,6 +76,48 @@ class Home extends Component {
         }
         /*********************************************************************** */
 
+
+        if (CarInfoReducer.sendCar.isExecStatus == 2) {
+            console.log('CarInfoReducer.sendCar执行完毕')
+            if (CarInfoReducer.sendCar.isResultStatus == 0) {
+                console.log('CarInfoReducer.sendCar执行成功')
+                this.getData()
+            }
+        }
+
+
+        if (CarInfoReducer.exportCar.isExecStatus == 2) {
+            console.log('CarInfoReducer.exportCar', '执行完毕')
+            if (CarInfoReducer.exportCar.isResultStatus == 0) {
+                console.log('CarInfoReducer.exportCar', '执行成功')
+                this.getData()
+            }
+        }
+
+
+        if (CarInfoReducer.importCar.isExecStatus == 2) {
+            console.log('CarInfoReducer.importCar', '执行完毕')
+            if (CarInfoReducer.importCar.isResultStatus == 0) {
+                console.log('CarInfoReducer.importCar', '执行成功')
+                this.getData()
+            }
+        }
+
+        if (CarInfoReducer.moveCar.isExecStatus == 2) {
+            console.log('CarInfoReducer.moveCar', '执行完毕')
+            if (CarInfoReducer.moveCar.isResultStatus == 0) {
+                console.log('CarInfoReducer.moveCar', '执行成功')
+                this.getData()
+            }
+        }
+
+        if (AddCarReducer.addCar.isExecStatus == 2) {
+            console.log('AddCarReducer.addCar', '执行完毕')
+            if (AddCarReducer.addCar.isResultStatus == 0) {
+                console.log('AddCarReducer.addCar执行成功')
+                this.getData()
+            }
+        }
     }
 
     onBarcodeReceived(param) {
@@ -88,7 +135,7 @@ class Home extends Component {
     }
 
     render() {
-       // console.log(this.props)
+        // console.log(this.props)
         let { storageList } = this.props.homeReducer.getStoragesHome.data
         let { recordList } = this.props.homeReducer.getRecordsHome.data
         return (
@@ -106,8 +153,10 @@ class Home extends Component {
 
 const mapStateToProps = (state) => {
     return {
+        CarInfoReducer: state.CarInfoReducer,
         userReducer: state.LoginReducer.user,
-        homeReducer: state.HomeReducer
+        homeReducer: state.HomeReducer,
+        AddCarReducer: state.AddCarReducer
     }
 }
 
