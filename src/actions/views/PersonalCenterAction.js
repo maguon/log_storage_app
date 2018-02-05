e9bf84bf6641dfb7c11eb371b8b41ce333910f05
rb yu <rbyu@rbdeMacBook-Air.local>
2018-02-05 08:46:50 +0800

    ok

--- src/actions/views/PersonalCenterAction.js
+++ src/actions/views/PersonalCenterAction.js
@@ -4,15 +4,15 @@ import * as actionTypes from '../../actionTypes'
 import { ObjectToUrl } from '../../util/ObjectToUrl'
 import { ToastAndroid } from 'react-native'
 
-export const updatePersonalImage = (param) => async (dispatch) => {   
+export const updatePersonalImage = (param) => async (dispatch) => {  
     dispatch({ type: actionTypes.personalCenterActionTypes.Update_PersonalImage_waiting, payload: {} })
     try {
-        const uploadUrl = `${file_host}/user/${param.uploadImage.requiredParam.userId}/image${ObjectToUrl(param.uploadImage.optionalParam)}`
-        const uploadUrlRes = await httpRequest.postFile(uploadUrl, param.uploadImage.postParam)
+        const uploadUrl = `${file_host}/user/${param.uploadImage.requiredParam.userId}/image?${ObjectToUrl(param.uploadImage.optionalParam)}`
+        const uploadUrlRes = await httpRequest.postFile(uploadUrl, param.uploadImage.postParam)        
         if (uploadUrlRes.success) {
             const updateAvatarImageUrl = `${base_host}/user/${param.updateAvatarImage.requiredParam.userId}/avatarImage`
             param.updateAvatarImage.putParam.avatarImage = uploadUrlRes.imageId
-            const updateAvatarImageRes = await httpRequest.put(updateAvatarImageUrl, param.updateAvatarImage.putParam)
+            const updateAvatarImageRes = await httpRequest.put(updateAvatarImageUrl, param.updateAvatarImage.putParam)        
             if(updateAvatarImageRes.success){
                 dispatch({ type: actionTypes.loginActionTypes.change_AvatarImage, payload: { avatar_image: uploadUrlRes.imageId } })
                 dispatch({ type: actionTypes.personalCenterActionTypes.Update_PersonalImage_success, payload: { } })
